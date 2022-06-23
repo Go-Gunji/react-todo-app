db3317a1c35ffcc41ca2f9227f85a81b05c14a19
jst-gunji <go.gunji@stream.co.jp>
2022-06-23 11:45:01 +0900

    入力されたToDoをリストで追加表示していく処理追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import { useState } from "react";
 
+type Todo = {
+  value: string;
+  id: number;
+};
+
 export const App = () => {
   /**
    * 追加するToDoの文字列ステート
    */
   const [text, setText] = useState("");
+  /**
+   * ToDoリストのステート
+   */
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleOnSubmit = () => {
+    // 何も入力されていなかったらリターン
+    if (!text) return;
+
+    // 新しい Todo を作成
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+    // フォームへの入力をクリアする
+    setText("");
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault}>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input type="submit" value="追加" onSubmit={(e) => e.preventDefault} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="追加" />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}> {todo.value}</li>;
+        })}
+      </ul>
     </div>
   );
 };
